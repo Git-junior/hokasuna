3f7c40cade25912a4fd574b52f1aa5aa37ce8bb7
Git-junior <juni0r.kiza6@gmail.com>
2022-07-11 18:21:18 +0900

    店舗新規登録結合テストコード

--- spec/system/stores_spec.rb
+++ spec/system/stores_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
+def basic_pass(path)
+  username = ENV["BASIC_AUTH_USER"]
+  password = ENV["BASIC_AUTH_PASSWORD"]
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
 RSpec.describe '店舗新規登録', type: :system do
   before do
     @store = FactoryBot.build(:store)
@@ -8,21 +14,45 @@ RSpec.describe '店舗新規登録', type: :system do
   context '店舗新規登録ができるとき' do
     it '正しい情報を入力すれば店舗新規登録ができてトップページへ移動する' do
       # ログインページへ移動する
+      basic_pass root_path
+      visit root_path
       # ログインページに新規登録ページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('Sign up')
       # 新規登録ページへ移動する
+      visit new_store_registration_path
       # 店舗情報を入力する
+      fill_in 'Name', with: @store.name
+      fill_in 'Email', with: @store.email
+      fill_in 'Password', with: @store.password
+      fill_in 'Password confirmation', with: @store.password_confirmation
       # Create Accountボタンを押すとStoreモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Store.count }.by(1)
       # トップページへ移動したことを確認する
+      expect(current_path).to eq(root_path)
     end
   end
   context '店舗新規登録ができないとき' do
     it '誤った情報では店舗新規登録ができずに新規登録ページへ戻ってくる' do
       # ログインページへ移動する
+      basic_pass root_path
+      visit root_path
       # ログインページに新規登録ページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('Sign up')
       # 新規登録ページへ移動する
+      visit new_store_registration_path
       # 店舗情報を入力する
+      fill_in 'Name', with: ''
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: ''
+      fill_in 'Password confirmation', with: ''
       # Create Accountボタンを押してもStoreモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Store.count }.by(0)
       # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq(store_registration_path)
     end
   end
 end
