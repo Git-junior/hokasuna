f883871e0eab725ea82b518853b1e9930c5998ec
Git-junior <juni0r.kiza6@gmail.com>
2022-07-11 18:09:40 +0900

    exampleの整理

--- spec/system/stores_spec.rb
+++ spec/system/stores_spec.rb
@@ -1,9 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "Stores", type: :system do
+RSpec.describe '店舗新規登録', type: :system do
   before do
-    driven_by(:rack_test)
+    @store = FactoryBot.build(:store)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context '店舗新規登録ができるとき' do
+    it '正しい情報を入力すれば店舗新規登録ができてトップページへ移動する' do
+      # ログインページへ移動する
+      # ログインページに新規登録ページへ遷移するボタンがあることを確認する
+      # 新規登録ページへ移動する
+      # 店舗情報を入力する
+      # Create Accountボタンを押すとStoreモデルのカウントが1上がることを確認する
+      # トップページへ移動したことを確認する
+    end
+  end
+  context '店舗新規登録ができないとき' do
+    it '誤った情報では店舗新規登録ができずに新規登録ページへ戻ってくる' do
+      # ログインページへ移動する
+      # ログインページに新規登録ページへ遷移するボタンがあることを確認する
+      # 新規登録ページへ移動する
+      # 店舗情報を入力する
+      # Create Accountボタンを押してもStoreモデルのカウントは上がらないことを確認する
+      # 新規登録ページへ戻されることを確認する
+    end
+  end
 end
