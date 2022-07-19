36b5462ca28447cbab3efe5bc634486e42ecd8d9
Git-junior <juni0r.kiza6@gmail.com>
2022-07-19 23:29:55 +0900

    exampleの整理

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @room = FactoryBot.build(:room)
+  end
+
+  describe 'チャットルーム作成' do
+    context '新規作成できるとき' do
+      it 'nameが存在すれば作成できる' do
+        
+      end
+    end
+    context '新規作成できないとき' do
+      it 'nameが空では作成できない' do
+        
+      end
+    end
+  end
 end
