f5713681c2d393c38f4e554d82f370301b2b223f
Git-junior <juni0r.kiza6@gmail.com>
2022-07-22 19:51:54 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :stores
   root to: "rooms#index"
   resources :stores, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
