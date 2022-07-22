399c36e6e3671731b54de139727ce6993386ca15
Git-junior <juni0r.kiza6@gmail.com>
2022-07-22 19:35:20 +0900

    messagesテーブルの作成

--- db/migrate/20220722101955_create_messages.rb
+++ db/migrate/20220722101955_create_messages.rb
@@ -1,7 +1,13 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-
+      t.string :ingredients
+      t.integer :quantity
+      t.boolean :preservation
+      t.datetime :expiry
+      t.text :comment
+      t.references :store, null: false, foreign_key: true
+      t.references :room, null: false, foreign_key: true 
       t.timestamps
     end
   end
