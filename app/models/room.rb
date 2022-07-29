6612776de378637840c6b89b4b38fd677cf3048c
Git-junior <juni0r.kiza6@gmail.com>
2022-07-29 15:38:00 +0900

    dependentオプションの設定（チャットルーム削除機能）

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
-  has_many :consultation
+  has_many :consultation, dependent: :destroy
   has_many :stores, through: :consultation
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
 end
