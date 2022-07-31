c97bccafb05718ed1ab4bfed0e928300a8a5dcde
Git-junior <juni0r.kiza6@gmail.com>
2022-07-31 20:05:04 +0900

    メッセージ投稿時にイベント発火させる設定

--- app/javascript/chat.js
+++ app/javascript/chat.js
@@ -1,5 +1,7 @@
 function post() {
-  
+  const submit = document.getElementsByClassName("form-submit")[0];
+  submit.addEventListener('click', () => {
+  });
 }
 
 window.addEventListener('load', post);
