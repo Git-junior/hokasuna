421dbbdafdb2e53f20f44b0a388bcc5cc03c3b39
Git-junior <juni0r.kiza6@gmail.com>
2022-08-01 18:22:51 +0900

    フォームに入力した値の取得

--- app/javascript/chat.js
+++ app/javascript/chat.js
@@ -1,6 +1,8 @@
 function post() {
   const submit = document.getElementsByClassName("form-submit")[0];
   submit.addEventListener('click', () => {
+    const form = document.getElementsByClassName("form")[0];
+    const formData = new FormData(form);
   });
 }
 
