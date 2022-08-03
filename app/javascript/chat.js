0d0df9e0b440008a78dc9eb638089b5f08a448e7
Git-junior <juni0r.kiza6@gmail.com>
2022-08-03 19:37:28 +0900

    非同期通信のためのリクエスト送信処理

--- app/javascript/chat.js
+++ app/javascript/chat.js
@@ -3,6 +3,10 @@ function post() {
   submit.addEventListener('click', () => {
     const form = document.getElementsByClassName("form")[0];
     const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/rooms/:room_id/messages", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
   });
 }
 
