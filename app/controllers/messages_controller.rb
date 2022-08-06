ace2ff2f1d51092c4ee49e2b0736bbbeca934d16
Git-junior <juni0r.kiza6@gmail.com>
2022-08-06 19:23:25 +0900

    コントローラーに送信されたリクエストの処理（レスポンスをJSON形式に変更）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,7 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      redirect_to room_messages_path(@room)
+      render json:{ message: @message }
     else
       @messages = @room.messages.includes(:store)
       render :index
