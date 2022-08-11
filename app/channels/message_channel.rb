be9c2d1de6d827a2facbdd6d94ee85e00aafe6b4
Git-junior <juni0r.kiza6@gmail.com>
2022-08-11 21:03:48 +0900

    チャネルの設定（データの経路の定義）

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -1,6 +1,8 @@
 class MessageChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    @room = Room.find(params[:room_id])
+    stream_for @room
   end
 
   def unsubscribed
