202b8cc713afa05674d86ce58b80fde1869eb852
Git-junior <juni0r.kiza6@gmail.com>
2022-08-12 22:54:21 +0900

    Messageモデル単体テストコード

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -8,18 +8,24 @@ RSpec.describe Message, type: :model do
   describe 'メッセージ投稿' do
     context 'メッセージが投稿できるとき' do
       it 'commentが存在すれば投稿できる' do
-        
+        expect(@message).to be_valid
       end
     end
     context 'メッセージが投稿できないとき' do
       it 'commentが空では投稿できない' do
-        
+        @message.comment = ''
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Comment can't be blank")
       end
       it 'storeが紐付いていなければ投稿できない' do
-        
+        @message.store = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Store must exist")
       end
       it 'roomが紐付いていなければ投稿できない' do
-        
+        @message.room = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Room must exist")
       end
     end
   end
