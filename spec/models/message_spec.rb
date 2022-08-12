4ba0ab180f76c2e008612f878db0a4dea3da5e8d
Git-junior <juni0r.kiza6@gmail.com>
2022-08-12 22:34:43 +0900

    exampleの整理

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = FactoryBot.build(:message)
+  end
+
+  describe 'メッセージ投稿' do
+    context 'メッセージが投稿できるとき' do
+      it 'commentが存在すれば投稿できる' do
+        
+      end
+    end
+    context 'メッセージが投稿できないとき' do
+      it 'commentが空では投稿できない' do
+        
+      end
+      it 'storeが紐付いていなければ投稿できない' do
+        
+      end
+      it 'roomが紐付いていなければ投稿できない' do
+        
+      end
+    end
+  end
 end
