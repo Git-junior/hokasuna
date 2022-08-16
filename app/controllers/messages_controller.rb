f191e2a4d0ca8ba182ba8c38fe23394108094c3a
Git-junior <juni0r.kiza6@gmail.com>
2022-08-16 23:33:21 +0900

    Rubocopの実行

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,9 +9,7 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      if @message.preservation.nil?
-        @message.preservation = ""
-      end
+      @message.preservation = '' if @message.preservation.nil?
       MessageChannel.broadcast_to @room, { message: @message, store: @message.store }
     else
       @messages = @room.messages.includes(:store)
