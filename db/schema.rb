fbc81542d7be98688617d72bbc91f4c9a9a3c6d5
Git-junior <juni0r.kiza6@gmail.com>
2022-08-26 19:00:24 +0900

    （※messagesテーブルのマイグレーション修正）

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2022_07_22_101955) do
     t.string "ingredients"
     t.string "quantity"
     t.string "preservation"
-    t.string "expiry"
+    t.string "expiry", default: "", null: false
     t.bigint "store_id", null: false
     t.bigint "room_id", null: false
     t.datetime "created_at", precision: 6, null: false
