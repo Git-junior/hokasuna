bf79d04380d7ba5041f30f26edd29ef5aafcdc0e
Git-junior <juni0r.kiza6@gmail.com>
2022-08-26 17:33:49 +0900

    （※検証解除）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,6 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      binding.pry
       @message.preservation = '' if @message.preservation.nil?
       MessageChannel.broadcast_to @room, { message: @message, store: @message.store }
     else
