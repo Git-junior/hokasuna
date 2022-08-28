cf270bc684d4b5ae3b4fd370ed0723445216b3f8
Git-junior <juni0r.kiza6@gmail.com>
2022-08-28 16:25:06 +0900

    Messageモデルのアソシエーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
-  validates :comment, presence: true
-
   belongs_to :store
   belongs_to :room
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :business
 end
