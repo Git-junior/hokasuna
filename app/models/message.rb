22021aecce18c7f0ab455925b901ba4d9dd659a5
Git-junior <juni0r.kiza6@gmail.com>
2022-08-31 21:20:04 +0900

    Messageモデルのバリデーション設定変更

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,5 @@ class Message < ApplicationRecord
   belongs_to :store
   belongs_to :room
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :business
-
-  validates :business_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :business, presence: true
 end
