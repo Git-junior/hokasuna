d5cde2ca64b0cfee7f1f733c74e3f2540d23fc5d
Git-junior <juni0r.kiza6@gmail.com>
2022-08-31 21:20:45 +0900

    Businessモデルの無効化

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,11 +1,11 @@
-class Business < ActiveHash::Base
-  self.data = [
-    { id: 1, name: '▼ 御用件を選択 ▼' },
-    { id: 2, name: '【依頼】譲って！！' },
-    { id: 3, name: '【依頼】貰って！！' },
-    { id: 4, name: '【返答】' }
-  ]
+# class Business < ActiveHash::Base
+#   self.data = [
+#     { id: 1, name: '▼ 御用件を選択 ▼' },
+#     { id: 2, name: '【依頼】譲って！！' },
+#     { id: 3, name: '【依頼】貰って！！' },
+#     { id: 4, name: '【返答】' }
+#   ]
 
-  include ActiveHash::Associations
-  has_many :messages
-end
+#   include ActiveHash::Associations
+#   has_many :messages
+# end
