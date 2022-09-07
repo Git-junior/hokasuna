cbfb9ca20b246d17514beddd33f3dd73bb86bd76
Git-junior <juni0r.kiza6@gmail.com>
2022-09-07 17:50:51 +0900

    exampleの整理

--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "Rooms", type: :system do
+RSpec.describe "チャットルーム新規作成", type: :system do
   before do
-    driven_by(:rack_test)
+    @store1 = FactoryBot.create(:store)
+    @store2 = FactoryBot.create(:store)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context '新規作成できるとき' do
+    it '正しい情報を入力すればチャットルーム新規作成ができてトップページへ移動する' do
+      # ログインする
+      # チャットルーム作成へのボタンがあることを確認する
+      # チャットルーム新規作成へ移動する
+      # ルーム情報を入力する
+      # Create Roomボタンを押すとRoomモデルのカウントが1上がることを確認する
+      # トップページへ移動したことを確認する
+    end
+  end
+  context '新規作成できないとき' do
+    it '誤った情報ではチャットルーム新規作成ができずに新規作成ページへ戻ってくる' do
+      # ログインする
+      # チャットルーム作成へのボタンがあることを確認する
+      # チャットルーム新規作成へ移動する
+      # ルーム情報を入力する
+      # Create Roomボタンを押してもRoomモデルのカウントは上がらないことを確認する
+      # 新規作成ページへ戻されることを確認する
+    end
+  end
 end
