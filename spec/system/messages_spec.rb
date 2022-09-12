7f44d6c952f5214d55e0ef5af3bcbd4d91bdd2fd
Git-junior <juni0r.kiza6@gmail.com>
2022-09-12 23:05:09 +0900

    （仮）メッセージ投稿結合テストコード（正常系）

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -8,10 +8,27 @@ RSpec.describe "メッセージ投稿", type: :system do
   context 'メッセージ投稿ができるとき' do
     it '正しい情報を入力すればメッセージ投稿ができて、投稿内容が表示されている' do
       # ログインする
+      log_in(@consultation.store)
       # 作成されたチャットルームへ移動する
+      click_on(@consultation.room.name)
       # メッセージ情報を入力する
+      select(value = '【依頼】譲って！！', from: 'message[business]')
+      fill_in 'message[ingredients]', with: '食材'
+      fill_in 'message[quantity]', with: '1個'
+      fill_in 'message[comment]', with: 'コメント'
+
+      # 【要修正】
       # 送信ボタンを押すとMessageモデルのカウントが1上がることを確認する
+      # expect {
+        # find('input[name="commit"]').click
+      # }.to change { Message.count }.by(1)
+
+      # 【仮】
+      # 送信ボタンを押す
+      find('input[name="commit"]').click
+
       # 送信した値がブラウザに表示されていることを確認する
+      expect(page).to have_content('コメント')
     end
   end
   context 'メッセージ投稿ができないとき' do
