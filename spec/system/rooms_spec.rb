d8f065132074d1bc6be8f5382a535e11612ca1c7
Git-junior <juni0r.kiza6@gmail.com>
2022-09-15 02:32:39 +0900

    チャットルーム削除結合テストコード

--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -53,9 +53,16 @@ RSpec.describe 'チャットルーム削除', type: :system do
 
   it 'チャットルームを削除すると、関連するメッセージが全て削除される' do
     # ログインする
+    log_in(@consultation.store)
     # 作成されたチャットルームへ移動する
+    click_on(@consultation.room.name)
     # メッセージ情報を2つDBに追加する
+    FactoryBot.create_list(:message, 2, room_id: @consultation.room.id, store_id: @consultation.store.id)
     # 「チャットを終了」ボタンをクリックすることで、作成した2つのメッセージが削除されていることを確認する
+    expect {
+      find_link('チャットを終了', href: room_path(@consultation.room)).click
+    }.to change { @consultation.room.messages.count }.by(-2)
     # トップページに遷移していることを確認する
+    expect(current_path).to eq(root_path)
   end
 end
